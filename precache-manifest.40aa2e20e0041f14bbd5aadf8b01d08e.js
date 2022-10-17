b06ffaf98f96ff9840057b63d71d3f3e34611a9e
webbbg <113067650+webbage@users.noreply.github.com>
2022-10-17 20:40:37 +0300

    Updates

---
+++ precache-manifest.40aa2e20e0041f14bbd5aadf8b01d08e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecdc9a92c54805163bcd7e02d91191c",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "d8bbe94d9aab3969a360",
+    "url": "/Keeper/static/js/2.6ee16f53.chunk.js"
+  },
+  {
+    "revision": "566c0232db8c74112afd",
+    "url": "/Keeper/static/js/main.cf64e888.chunk.js"
+  },
+  {
+    "revision": "4b772ed70d71f9d0beff",
+    "url": "/Keeper/static/js/runtime-main.fd6e374a.js"
+  }
+]);
